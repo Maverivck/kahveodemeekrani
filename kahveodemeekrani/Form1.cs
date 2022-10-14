d2816ccbd997ce241cf49b98427e35070ded9acf
Maverick <89163711+Maverivck@users.noreply.github.com>
2022-10-14 11:58:25 +0300

    Add project files.

---
+++ kahveodemeekrani/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kahveodemeekrani
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string kull, sifre;
+            kull = textBox1.Text;
+            sifre = textBox2.Text;
+
+
+
+            if (kull == "kerem" && sifre == "1234")
+            {
+                MessageBox.Show("Giriş Başarılı");
+                Form2 f2 = new Form2();
+                f2.Show();
+                this.Hide();
+
+            }
+            else if (kull != "kerem")
+            {
+                MessageBox.Show("Kullanıcı Adı Hatalı");
+                Application.Exit();
+            }
+            else if (sifre != "1234")
+            {
+                MessageBox.Show("Şifre Hatalı");
+                Application.Exit();
+            }
+            else
+            {
+                MessageBox.Show("Giriş Başarısız");
+            }
+        }
+    }
+}
---
+++ kahveodemeekrani/Form2.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kahveodemeekrani
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+        int caysayisi = 0;
+        int coldsayisi = 0;
+        int susayisi = 0;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int coldsayisi;
+            coldsayisi = Convert.ToInt32(label4.Text);
+            coldsayisi++;
+            label4.Text = Convert.ToString(coldsayisi);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int caysayisi = Convert.ToInt32(label5.Text);
+
+
+            caysayisi++;
+            label5.Text = Convert.ToString(caysayisi);
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            int susayisi = Convert.ToInt32(label6.Text);
+            susayisi++;
+            label6.Text = Convert.ToString(susayisi);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            int coldsayisi = Convert.ToInt32(label4.Text);
+            if (coldsayisi > 0)
+            {
+                coldsayisi--;
+                label4.Text = Convert.ToString(coldsayisi);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int caysayisi = Convert.ToInt32(label5.Text);
+            if (caysayisi > 0)
+            {
+                caysayisi--;
+                label5.Text = Convert.ToString(caysayisi);
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            int susayisi = Convert.ToInt32(label6.Text);
+            if (susayisi > 0)
+            {
+                susayisi--;
+                label6.Text = Convert.ToString(susayisi);
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            caysayisi = 0;
+            coldsayisi = 0;
+            susayisi = 0;
+
+            label4.Text = "0";
+            label5.Text = "0";
+            label6.Text = "0";
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Sipariş Özeti" + "\n Cold Brew : " + label4.Text + "\n Çay :" + label5.Text + "\n Su : " + label6.Text);
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Ödeme Başarılı bir şekilde tamamlandı");
+
+            Application.Exit();
+        }
+    }
+}
